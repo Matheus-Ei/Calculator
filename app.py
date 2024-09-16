cb3058dfc4af9fb97cf4a11c0cde690ac04ea0a1
Matheus-Ei <t4igacomercial@gmail.com>
2024-09-16 14:20:39 -0300

    add the updater

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from window import Window
+from update import Update
+import subprocess
 
 
 def run():
+    result = subprocess.run(['git', 'fetch'], capture_output=True, text=True)
+    if (result.stdout == ""):
+        pass
+    else:
+        updater = Update()
+        updater.run()
+
     root = Window()
     root.run()
 
---
+++ update.py
@@ -0,0 +1,77 @@
+import sys
+import subprocess
+from PyQt5.QtWidgets import (QApplication,
+                             QWidget,
+                             QVBoxLayout,
+                             QLineEdit,
+                             QPushButton,
+                             QLabel)
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
+
+
+class Update(QWidget):
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        super().__init__()
+
+    def __make(self):
+        self.setWindowTitle("Calculadora")
+        self.setGeometry(300, 300, 300, 200)
+
+    def __add_text(self, layout, text, font="Arial", size=24):
+        label = QLabel(text, self)
+        font_element = QFont(font, size)
+        label.setFont(font_element)
+        label.setGeometry(0, 0, 400, 300)
+        label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(label)
+
+        return label
+
+    def __add_button(self, layout, text, function):
+        button = QPushButton(text, self)
+        button.clicked.connect(function)
+        layout.addWidget(button)
+
+    def __add_input(self, layout, placeholder):
+        input_field = QLineEdit(self)
+        input_field.setPlaceholderText(placeholder)
+        layout.addWidget(input_field)
+
+        return input_field
+
+    def __add_widgets(self):
+        layout = QVBoxLayout()
+
+        self.header = self.__add_text(layout, "Update disponivel!")
+
+        self.__add_text(layout, "Atualizar a calculadora?", size=8)
+        self.__add_button(layout, "Sim", self.__relaunch)
+        self.__add_button(layout, "Não", self.close)
+
+        self.setLayout(layout)
+
+    def __update(self):
+        subprocess.run(['git', 'pull'])
+
+    def __relaunch(self):
+        self.__update()
+
+        subprocess.Popen(["python3", "./app.py"],
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+
+        exit()
+
+    def run(self):
+        self.__make()
+        self.__add_widgets()
+
+        self.show()
+        sys.exit(self.app.exec_())
+
+
+if __name__ == "__main__":
+    app = Update()
+    app.run()
